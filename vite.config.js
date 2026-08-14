1d6ef569ca1c55d81e2d1420dc8962acf643ae3e
Krueck <130928894+Krueck@users.noreply.github.com>
2026-08-14 15:25:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // WICHTIG: Exakt so schreiben wie auf GitHub (CacheNJump)!
-  base: '/CacheNJump/', 
+  // Der Pfad MUSS exakt mit dem GitHub-Repository-Namen übereinstimmen:
+  base: '/CacheNJump/',
 });
