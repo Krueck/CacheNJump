0a69504eab3ba6b9b5f26762d75d0a767b4a1fb4
Krueck <130928894+Krueck@users.noreply.github.com>
2026-08-16 16:10:59 +0000

    Merge-Konflikt in vite.config.js behoben

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-export default defineConfig({
-  // Der Pfad MUSS exakt mit dem GitHub-Repository-Namen übereinstimmen:
-  base: '/CacheNJump/',
-});
-=======
->>>>>>> 80daba9
 export default defineConfig(({ command }) => {
   return {
-    // Im Dev-Modus (npm run dev) nutzen wir '/', beim Build (npm run build) den Unterordner
+    // Im Dev-Modus ('npm run dev') nutzen wir '/', beim Build ('npm run build') den Unterordner '/CacheNJump/'
     base: command === 'serve' ? '/' : '/CacheNJump/',
   }
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> 6b5a020 (Spielerauswahl und Mosquitos hinzugefügt)
->>>>>>> 80daba9
